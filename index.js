bffd61e98234e2bd75d658333313af6ca6402884
Allan <allcreativewrites@gmail.com>
2023-09-25 15:08:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(array, driverName) {
+    const firstLetterCapitalized = driverName.charAt(0).toUpperCase() + driverName.slice(1).toLowerCase();
+    const lowerCase = driverName.toLowerCase();
+    const filter = array.filter(value => value === firstLetterCapitalized || value === lowerCase );
+    return filter;
+}
+
+function fuzzyMatch(array, driverName) {
+    const filterArray = array.filter(value => value.slice(0,2) === driverName);
+    return filterArray;
+}
+
+function matchName(array, driverName) {
+    const filterArray = array.filter(value => value.name === driverName);
+    return filterArray;
+}
